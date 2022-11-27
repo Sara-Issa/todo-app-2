07668fb1043a3a24a3d6cd3ac3fbeb9928066c46
Moktar Mohamed <saraisamo123@gmail.com>
2022-11-27 11:16:17 +0300

    compleated todo

--- script.js
+++ script.js
@@ -1,20 +1,69 @@
 // add todo List
 var data = JSON.parse(localStorage.getItem("todos"));
-data.push();
-JSON.stringify(localStorage.setItem("todos"));
+localStorage.setItem("todos", JSON.stringify(data));
 function add() {
-  var obj = {
-    todo: "hi",
-    status: "undone",
-  };
-
+  var input = document.querySelector(".todo-input");
+  if (input.value == "") {
+    alert("Please fill the empty place");
+  } else {
+    var obj = {
+      todo: input.value,
+      status: "undone",
+    };
+  }
   console.log(obj);
   var data = JSON.parse(localStorage.getItem("todos")) || [];
   data.push(obj);
-  JSON.stringify(localStorage.setItem("todos"));
+  localStorage.setItem("todos", JSON.stringify(data));
+  show();
 }
-// show todo List
 
+window.addEventListener("load", function () {
+  show();
+});
+
+// show todo List
+function show() {
+  var card = document.querySelector(".ul");
+  card.innerHTML = "";
+  var value = JSON.parse(localStorage.getItem("todos"));
+
+  for (let i = 0; i < value.length; i++) {
+    var done = value[i].status == "undone" ? "" : "checked";
+    var line = value[i].status == "undone" ? "" : "line-through";
+    var object = value[i];
+    card.innerHTML += `<li class="li" >
+    <input
+      type="checkbox"
+      class="check-input"
+      onclick="update(${i});"
+      name="vehicle1"
+      ${done}
+    /><label class="${line} todo-label" for="">${object.todo}</label>
+    <button type="button" class="delete-btn" onclick="del(${i});" >
+      <i class="fa-solid fa-trash"></i>
+    </button>
+  </li>`;
+  }
+}
 // update todo List
+function update(i) {
+  var value = JSON.parse(localStorage.getItem("todos"));
+  var status = value[i].status;
+  if (status == "undone") {
+    status = value[i].status = "done";
+    localStorage.setItem("todos", JSON.stringify(value));
+  } else if (status == "done") {
+    status = value[i].status = "undone";
+    localStorage.setItem("todos", JSON.stringify(value));
+  }
+  show();
+}
 
 // delete todo List
+function del(i) {
+  var value = JSON.parse(localStorage.getItem("todos"));
+  value.splice(i, 1);
+  localStorage.setItem("todos", JSON.stringify(value));
+  show();
+}
